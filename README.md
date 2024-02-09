ff067fbb875e8931631e5fe01a407024b54c3a86
Nick Cote <ncote@ucar.edu>
2024-02-09 15:14:10 -0700

    fresh start

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # docker-how-to
-A quick tutorial on containerizing a basic Flask application
+
+A tutorial on containerizing a basic Flask application. 
+
+***Note*** Fork this repository and follow the how-to to build everything yourself
+
+There is a how-to.ipynb file that walks though the following steps: 
+
+    - How to run the application locally 
+    - Writing a container file 
+    - Creating a GitHub actions workflow to build the image
+    - Configuring automation to deploy your application to the CISL Cloud Pilot cluster. 
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Import the flask python module
-from flask import Flask
+from flask import Flask, render_template
 
 # Define the Flask constructor
 app = Flask(__name__)
@@ -7,8 +7,12 @@ app = Flask(__name__)
 # The .route() function in flask is a decorator that tells the application what function to run when visiting certain URLs.
 # This defines that when someone visits <URL>/ the hello_world application should run
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def home():
+    return render_template("home.html")
+
+@app.route('/templates/navbar.html')
+def navbar():
+    return render_template('navbar.html')
 
 # Create the main driver function to run the app
 if __name__ == '__main__':
